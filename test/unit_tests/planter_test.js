be19bfa3af8afea1723cc976a0a11d7c7a2f3726
Taka Okunishi <okunishinishi@gmail.com>
2015-06-05 08:17:31 +0900

    add planter test

--- test/unit_tests/planter_test.js
+++ test/unit_tests/planter_test.js
@@ -3,10 +3,22 @@
  * Runs with nodeunit.
  */
 
-var Planter = require('../../lib/planter.js');
+var Planter = require('../../lib/planter.js')
+    ;
 
 exports['Create planter.'] = function (test) {
     var planter = new Planter();
     test.ok(planter);
     test.done();
+};
+
+exports['Do blossom.'] = function (test) {
+    var planter = new Planter();
+    var src = __dirname + '/../mocks/mock-bud.bud'
+    planter.blossom([
+        src
+    ], {}, function (err) {
+        test.ifError(err);
+        test.done();
+    });
 };
