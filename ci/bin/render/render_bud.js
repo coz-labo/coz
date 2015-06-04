bc35d30b723d279200b6608875f15a75a3fe0f2b
Taka Okunishi <okunishinishi@gmail.com>
2015-06-05 00:20:33 +0900

    add bud test

---
+++ ci/bin/render/render_bud.js
@@ -0,0 +1,23 @@
+#!/usr/bin/env node
+
+/**
+ * Render bud files.
+ */
+
+"use strict";
+
+var path = require('path');
+var planter = require('../../../lib');
+
+var basedir = path.resolve(__dirname, '../../..');
+process.chdir(basedir);
+
+planter.blossom([
+    'lib/**/.*.bud'
+], {
+    verbose: true
+}, function (e) {
+    if (e) {
+        console.error(e);
+    }
+});
---
+++ test/unit_tests/bud_test/bud_config_test.js
@@ -0,0 +1,18 @@
+/**
+ * Test case for bud_config.js
+ * Runs with nodeunit.
+ */
+
+"use strict";
+
+var BudConfig = require('../../../lib/bud/bud_config.js');
+
+exports['Create bud config.'] = function (test) {
+    var config = new BudConfig({
+        path: 'foo/bar'
+    });
+    test.ok(config);
+    var config2 = new BudConfig(config);
+    test.ok(config2);
+    test.done();
+};
