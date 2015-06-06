ecd77ab52a3a130ebb28411e8727a7eb4c4e8d24
Taka Okunishi <okunishinishi@gmail.com>
2015-06-06 11:48:38 +0900

    remove old engines

---
+++ test/unit_tests/template_test/resolve_engine_test.js
@@ -0,0 +1,19 @@
+/**
+ * Test case for module:leaf/lib/template/resolveEngine
+ * Runs with nodeunit.
+ */
+
+"use strict";
+
+var resolveEngine = require('../../../lib/template/resolve_engine');
+
+exports['Resolve a engine.'] = function (test) {
+    test.ok(resolveEngine('handlebars'));
+    test.ok(resolveEngine('Handlebars'));
+    test.ok(resolveEngine('handlebarsEngine'));
+    test.ok(resolveEngine('HandlebarsEngine'));
+    test.throws(function () {
+        resolveEngine('__invalid name__');
+    });
+    test.done();
+};
