239958e6657649a462433cee08281cdf46a8fc61
Taka Okunishi <okunishinishi@gmail.com>
2015-06-08 00:01:15 +0900

    add favicon

--- apiguide/assets/images/.resize.sh
+++ apiguide/assets/images/.resize.sh
@@ -9,4 +9,5 @@ HERE=$(cd "$(dirname $0)" && pwd)
 cd ${HERE}
 
 convert -resize x256 .src/coz-banner-src.png coz-banner.png
+convert -resize x128 .src/coz-favicon-src.png coz-favicon.png
 convert -resize x512 .src/coz-outline-src.jpg coz-outline.jpg
