9b231abedd8a8daae90cdf10540736c37abcb22e
Taka Okunishi <okunishinishi@gmail.com>
2015-06-08 19:51:24 +0900

    add wiki

--- ci/bin/deploy/deploy_wiki.sh
+++ ci/bin/deploy/deploy_wiki.sh
@@ -9,15 +9,10 @@ BASE_DIR=$(cd "${HERE}/../../.." && pwd)
 
 cd ${BASE_DIR}
 
-git submodule init
-git submodule update
 
 cd ${BASE_DIR}/docs/wiki/
 
-git pull
-git merge origin/master
-git submodule update
-
+git add . -A
 git commit -am "Update wiki"
 
 if [[ -n $(git diff --stat --cached origin/master) ]]; then
