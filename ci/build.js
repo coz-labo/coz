74bdaf3557dc6dca385a75aac4b66e828efc3ee1
Taka Okunishi <okunishinishi@gmail.com>
2015-08-13 11:46:16 +0900

    Version incremented to 2.0.1

--- ci/build.js
+++ ci/build.js
@@ -2,16 +2,12 @@
 
 var path = require('path'),
     apeTasking = require('ape-tasking'),
-    execcli = require('execcli'),
     coz = require('../lib');
 
 var basedir = path.resolve(__dirname, '..');
 process.chdir(basedir);
 
 apeTasking.runTasks('build', [
-    function (callback) {
-        execcli('./docs/examples/render_examples.sh', callback);
-    },
     function (callback) {
         coz.render([
             '.*.bud',
--- ci/doc.js
+++ ci/doc.js
@@ -7,16 +7,34 @@
 "use strict";
 
 var apeTasking = require('ape-tasking'),
+    async = require('async'),
     path = require('path'),
     coz = require('../lib'),
+    filecopy = require('filecopy'),
     apiguide = require('apiguide'),
+    mkdirp = require('mkdirp'),
+    cozExamples = require('coz-examples'),
     execcli = require('execcli');
 
 var basedir = path.resolve(__dirname, '..');
 process.chdir(basedir);
 
 apeTasking.runTasks('doc', [
-    function (callback) {
+    function generateExamples(callback) {
+        var examplesDir = path.join(require.resolve('coz-examples/package.json'), '..');
+        async.eachSeries(['.*.*', '*.*'], function (pattern, callback) {
+            async.eachSeries(Object.keys(cozExamples), function (dirname, callback) {
+                var src = path.join(examplesDir, dirname, pattern),
+                    destDir = path.join('docs/examples', dirname);
+
+                mkdirp.sync(destDir);
+                filecopy(src, destDir, {
+                    mkdirp: true
+                }, callback);
+            }, callback);
+        }, callback);
+    },
+    function generateApiGuide(callback) {
         apiguide([
             'lib/**/*.js',
             'README.md'
