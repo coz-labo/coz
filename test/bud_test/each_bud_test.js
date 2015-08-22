4545d5e8398f340e6050af42a2cb8eeaf9663b8e
Taka Okunishi <okunishinishi@gmail.com>
2015-08-22 22:02:57 +0900

    Version incremented to 3.0.5

---
+++ test/bud_test/each_bud_test.js
@@ -0,0 +1,23 @@
+/**
+ * Test case for eachBud.
+ * Runs with nodeunit.
+ */
+
+var eachBud = require('../../lib/bud/each_bud.js');
+
+exports.setUp = function(done) {
+    done();
+};
+
+exports.tearDown = function(done) {
+    done();
+};
+
+exports['Each bud'] = function(test){
+    eachBud(__dirname + '/*.js', function (filename, callback) {
+        callback(null);
+    }, function (err) {
+        test.ifError(err);
+        test.done();
+    });};
+
