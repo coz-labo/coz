bea9f6fbc6de6eff30d7f67d5aef8fb5029d4f9f
Taka Okunishi <okunishitaka.com@gmail.com>
2016-05-02 10:32:25 +0900

    Version incremented to 5.1.5

--- example/06-customize-coz/render-with-custom-tmpl.js
+++ example/06-customize-coz/render-with-custom-tmpl.js
@@ -5,31 +5,31 @@
  * This is an executable file for "examples/06-customize-coz"
  */
 
-'use strict'
-const Coz = require('coz').Coz
+
+var Coz = require('coz').Coz;
 
 // Create a custom coz context.
-let coz = new Coz({
-  // Define custom templates.
-  tmpls: {
-    // Custom template to generate single line json string.
-    singleLineJson (data) {
-      return JSON.stringify(data, null, 0)
+var coz = new Coz({
+    // Define custom templates.
+    tmpls: {
+        // Custom template to generate single line json string.
+        singleLineJson: function (data) {
+            return JSON.stringify(data, null, 0);
+        }
     }
-  }
-})
+});
 
 coz.render({
-  force: true,
-  mode: '444',
-  path: 'render-by-my-custom-tmpl-01.json',
-  // Use custom tmpl
-  tmpl: 'singleLineJson',
-  // Data to pass custom tmpl.
-  data: {
-    'generator': __filename,
-    'coz is': 'wonderful'
-  }
+    force: true,
+    mode: '444',
+    path: 'render-by-my-custom-tmpl-01.json',
+    // Use custom tmpl
+    tmpl: 'singleLineJson',
+    // Data to pass custom tmpl.
+    data: {
+        'generator': __filename,
+        'coz is': 'wonderful'
+    }
 }, function (err) {
-  console.log('Compile done with custom tmpl.');
-})
+    console.log('Compile done with custom tmpl.');
+});
