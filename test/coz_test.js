7e06bc9a4472c5e53a2b6484cd1cc16eebd2283f
Taka Okunishi <okunishitaka.com@gmail.com>
2016-05-18 14:26:25 +0900

    Bugfix

--- test/coz_test.js
+++ test/coz_test.js
@@ -11,87 +11,89 @@ const fs = require('fs')
 const injectmock = require('injectmock')
 const assert = require('assert')
 
-before((done) => {
-  injectmock(console, 'log', injectmock.noop)
-  mkdirp.sync(`${__dirname}/../tmp`)
-  done()
-})
+describe('coz', function () {
+  before((done) => {
+    // injectmock(console, 'log', injectmock.noop)
+    mkdirp.sync(`${__dirname}/../tmp`)
+    done()
+  })
 
-after((done) => {
-  injectmock.restoreAll()
-  done()
-})
+  after((done) => {
+    injectmock.restoreAll()
+    done()
+  })
 
-it('Create coz.', (done) => {
-  let coz = new Coz()
-  assert.ok(coz)
-  done()
-})
+  it('Create coz.', (done) => {
+    let coz = new Coz()
+    assert.ok(coz)
+    done()
+  })
 
-it('Do render.', () => co(function * () {
-  let coz = new Coz()
-  let src = `${__dirname}/../doc/mockups/mock-bud.bud`
-  yield coz.render([
-    src
-  ], { verbose: true })
-}))
+  it('Do render.', () => co(function * () {
+    let coz = new Coz()
+    let src = `${__dirname}/../doc/mockups/mock-bud.bud`
+    yield coz.render([
+      src
+    ], { verbose: true, prefix: 'hogehoge' })
+  }))
 
-it('Do render without options.', () => co(function * () {
-  let coz = new Coz()
-  let src = `${__dirname}/../doc/mockups/mock-bud.bud`
-  yield coz.render([
-    src
-  ])
-}))
+  it('Do render without options.', () => co(function * () {
+    let coz = new Coz()
+    let src = `${__dirname}/../doc/mockups/mock-bud.bud`
+    yield coz.render([
+      src
+    ])
+  }))
 
-it('Do clean.', () => co(function * () {
-  let coz = new Coz()
-  let filename = `${__dirname}/../tmp/some_file.txt.bud`
-  require('fs').writeFileSync(filename, 'module.exports = {path:__filename}')
-  yield coz.clean(filename)
-}))
+  it('Do clean.', () => co(function * () {
+    let coz = new Coz()
+    let filename = `${__dirname}/../tmp/some_file.txt.bud`
+    require('fs').writeFileSync(filename, 'module.exports = {path:__filename}')
+    yield coz.clean(filename)
+  }))
 
-it('With custom setup.', () => co(function * () {
-  let coz = new Coz()
-  let filename = `${__dirname}/../tmp/some_coz_file0008.txt`
-  yield coz.render({
-    path: filename,
-    force: true,
-    mkdirp: true,
-    tmpl: '{{myCustomHelper baz}}',
-    setup: {
-      helpers: {
-        myCustomHelper (txt) {
-          return 'my-custom-' + txt
+  it('With custom setup.', () => co(function * () {
+    let coz = new Coz()
+    let filename = `${__dirname}/../tmp/some_coz_file0008.txt`
+    yield coz.render({
+      path: filename,
+      force: true,
+      mkdirp: true,
+      tmpl: '{{myCustomHelper baz}}',
+      setup: {
+        helpers: {
+          myCustomHelper (txt) {
+            return 'my-custom-' + txt
+          }
         }
+      },
+      data: {
+        baz: 'quz'
       }
-    },
-    data: {
-      baz: 'quz'
-    }
-  })
-  let content = fs.readFileSync(filename).toString()
-  assert.equal('my-custom-quz', content.trim())
-}))
+    })
+    let content = fs.readFileSync(filename).toString()
+    assert.equal('my-custom-quz', content.trim())
+  }))
 
-it('Do render with configuration.', () => co(function * () {
-  let configuration = require.resolve('../doc/mockups/mock-coz-configuration')
-  let coz = new Coz(configuration)
-  let filename = `${__dirname}/../tmp/some_file2.txt`
-  if (fs.existsSync(filename)) {
-    fs.unlinkSync(filename)
-  }
-  assert.ok(coz)
-  yield coz.render({
-    path: filename,
-    force: true,
-    engine: 'myCustomEngine',
-    tmpl: 'foobarbaz'
-  })
-  fs.readFile(filename, function (err, content) {
-    assert.ifError(err)
-    assert.equal(String(content).trim(), 'renderByMyCustom')
-  })
-}))
+  it('Do render with configuration.', () => co(function * () {
+    let configuration = require.resolve('../doc/mockups/mock-coz-configuration')
+    let coz = new Coz(configuration)
+    let filename = `${__dirname}/../tmp/some_file2.txt`
+    if (fs.existsSync(filename)) {
+      fs.unlinkSync(filename)
+    }
+    assert.ok(coz)
+    yield coz.render({
+      path: filename,
+      force: true,
+      engine: 'myCustomEngine',
+      tmpl: 'foobarbaz'
+    })
+    fs.readFile(filename, function (err, content) {
+      assert.ifError(err)
+      assert.equal(String(content).trim(), 'renderByMyCustom')
+    })
+  }))
+})
 
 /* global describe, before, after, it */
