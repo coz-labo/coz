c8d86c622b01790d42a2c66049390220c7d2d7a9
okunishinishi <okunishinishi@gmail.com>
2020-01-26 20:56:06 +0900

    update deps

--- ci/doc.js
+++ ci/doc.js
@@ -8,7 +8,6 @@
 
 const apeTasking = require('ape-tasking')
 const path = require('path')
-const co = require('co')
 const coz = require('../shim/node')
 const filecopy = require('filecopy')
 const mkdirp = require('mkdirp')
@@ -17,18 +16,18 @@ const cozExamples = require('coz-examples')
 process.chdir(`${__dirname}/..`)
 
 apeTasking.runTasks('doc', [
-  () => co(function * () {
+  async () => {
     let examplesDir = path.join(require.resolve('coz-examples/package.json'), '..')
-    for (let pattern of [ '.*.*', '*.*' ]) {
+    for (let pattern of ['.*.*', '*.*']) {
       for (let dirname of Object.keys(cozExamples)) {
         let src = path.join(examplesDir, dirname, pattern)
         let destDir = path.join('example', dirname)
         mkdirp.sync(destDir)
-        yield filecopy(src, destDir, {
+        await filecopy(src, destDir, {
           mkdirp: true
         })
       }
     }
-  }),
+  },
   () => coz.render('doc/guides/.*.bud')
 ], true)
--- test/resolve_test.js
+++ test/resolve_test.js
@@ -6,21 +6,12 @@
 
 const resolve = require('../lib/resolve.js')
 const assert = require('assert')
-const co = require('co')
-
 describe('resolve', () => {
-  before(() => co(function * () {
 
-  }))
-
-  after(() => co(function * () {
-
-  }))
-
-  it('Resolve', () => co(function * () {
-    let buds = yield resolve(`${__dirname}/*.js`)
+  it('Resolve', async () => {
+    let buds = resolve(`${__dirname}/*.js`)
     assert.ok(buds)
-  }))
+  })
 })
 
 /* global describe, before, after, it */
